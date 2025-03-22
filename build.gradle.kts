080d4e494caf1dbec8b1260c5c8f613f0d48b658
minjun <hitobi1014@gmail.com>
2025-03-22 17:56:43 +0900

    chore. 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.allopen") version "1.9.25"
+
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "io.runnershigh"
@@ -20,17 +23,49 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // DB
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+    // Querydsl Logging
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+
+    // Development Tools
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Logging
+    implementation("org.springframework.boot:spring-boot-starter-logging") // 스프링 부트 로깅 스타터
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5") // 코틀린 로깅 라이브러리
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core") // Mockito 제외
+    }
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
+
+    // MockK 관련 라이브러리
+    testImplementation("io.mockk:mockk:1.13.9") // MockK 코어 라이브러리
+    testImplementation("com.ninja-squad:springmockk:4.0.2") // Spring 통합용 MockK
 }
 
 kotlin {
+    jvmToolchain(17)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
@@ -40,6 +75,10 @@ allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
     annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
 }
 
 tasks.withType<Test> {
