942e4b023c9779e9bba5986f05d76c74a3b1b1d7
minjun <hitobi1014@gmail.com>
2025-03-30 01:41:18 +0900

    feat. 로그인한 user 엔티티 가져오기

---
+++ src/main/kotlin/io/runnershigh/backend/user/util/LoginUserContext.kt
@@ -0,0 +1,23 @@
+package io.runnershigh.backend.user.util
+
+import io.runnershigh.backend.user.infrastructure.entity.UserEntity
+import io.runnershigh.backend.user.repository.query.UserQueryRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class LoginUserContext(
+    private val userQueryRepository: UserQueryRepository,
+) {
+    private fun getCurrentContextUserId(): Int {
+        val authentication = SecurityContextHolder.getContext().authentication
+        return authentication.name?.toInt() ?: throw IllegalStateException("User not found")
+    }
+
+    fun getCurrentUser(): UserEntity {
+        val userId = getCurrentContextUserId()
+        return userQueryRepository.findById(userId)
+            .orElseThrow { IllegalStateException("User not found") } // TODO 예외 변경하기
+            ?: throw IllegalStateException("User not found")
+    }
+}
