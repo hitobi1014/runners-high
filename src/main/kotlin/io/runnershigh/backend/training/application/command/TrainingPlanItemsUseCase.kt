d4b9292523bd2c532b8892e53612a4497b92e1b9
minjun <hitobi1014@gmail.com>
2025-04-07 22:24:58 +0900

    refactor. 훈련상세계획 useCase 수정
    - 저장한 값을 클라이언트에서 활용하기 위해 createTrainingItems 반환값 추가
    - schedule 엔티티 조회, spring-data-commons 의존 추가하여 코틀린 친화적 메서드로 변경 findById -> findByIdOrNull

--- src/main/kotlin/io/runnershigh/backend/training/application/command/TrainingPlanItemsUseCase.kt
+++ src/main/kotlin/io/runnershigh/backend/training/application/command/TrainingPlanItemsUseCase.kt
@@ -1,10 +1,12 @@
 package io.runnershigh.backend.training.application.command
 
 import io.runnershigh.backend.training.domain.mapper.toEntity
+import io.runnershigh.backend.training.infrastructure.entity.TrainingPlanItems
 import io.runnershigh.backend.training.infrastructure.entity.TrainingSchedules
 import io.runnershigh.backend.training.infrastructure.repository.command.TrainingPlanItemsRepository
 import io.runnershigh.backend.training.infrastructure.repository.command.TrainingSchedulesCommandRepository
 import io.runnershigh.backend.training.ui.dto.SaveTrainingPlanItem
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,14 +17,15 @@ class TrainingPlanItemsUseCase(
     private val trainingSchedulesRepository: TrainingSchedulesCommandRepository,
 ) {
 
-    fun createTrainingItems(scheduleId: Long, dto: SaveTrainingPlanItem) {
+    fun createTrainingItems(scheduleId: Long, dto: SaveTrainingPlanItem): TrainingPlanItems {
         val entity = dto.toEntity(getSchedule(scheduleId))
-        trainingPlanItemsRepository.save(entity)
+        return trainingPlanItemsRepository.save(entity)
     }
 
     private fun getSchedule(scheduleId: Long): TrainingSchedules {
         // TODO 에러 메세지 변경
-        return trainingSchedulesRepository.findById(scheduleId)
-            .orElseThrow { IllegalArgumentException("Schedule with id $scheduleId not found") }
+        return trainingSchedulesRepository.findByIdOrNull(scheduleId)
+            ?: throw IllegalArgumentException("Schedule not found")
+
     }
 }
