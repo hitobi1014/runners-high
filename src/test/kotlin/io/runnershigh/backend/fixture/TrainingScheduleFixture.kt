a22910358a102b38f3e11fb15726b129c6d8dc04
minjun <hitobi1014@gmail.com>
2025-04-07 22:25:55 +0900

    test. 훈련 상세계획 테스트
    - TrainingScheduleFixture, 간편하게 훈련일정 엔티티 사용하기 위해 객체 추가
    - 훈련 상세계획 정상 저장만 테스트

---
+++ src/test/kotlin/io/runnershigh/backend/fixture/TrainingScheduleFixture.kt
@@ -0,0 +1,28 @@
+package io.runnershigh.backend.fixture
+
+import io.runnershigh.backend.training.domain.enum.TrainingStatus
+import io.runnershigh.backend.training.infrastructure.entity.TrainingSchedules
+import io.runnershigh.backend.user.infrastructure.entity.UserEntity
+import java.time.LocalDate
+
+object TrainingScheduleFixture {
+    fun createDefault(
+        id: Long = 1L,
+        user: UserEntity = UserFixture.createDefault(),
+        title: String = "하프마라톤 대비",
+        location: String = "보라매 공원",
+        description: String = "보라매 공원 가볍게 러닝",
+        scheduledDate: LocalDate = LocalDate.now(),
+        status: TrainingStatus = TrainingStatus.PLANNED,
+    ): TrainingSchedules {
+        return TrainingSchedules(
+            id = id,
+            user = user,
+            title = title,
+            location = location,
+            description = description,
+            scheduledDate = scheduledDate,
+            status = status,
+        )
+    }
+}
