ac7538dddc55a33efba7a225c742546effa36525
minjun <hitobi1014@gmail.com>
2025-05-11 16:46:04 +0900

    [feat] Add controller 성공(조회, 수정, 삭제) 메시지 추가

--- src/main/kotlin/io/runnershigh/backend/shared/response/CommonResponseMessage.kt
+++ src/main/kotlin/io/runnershigh/backend/shared/response/CommonResponseMessage.kt
@@ -3,6 +3,8 @@ package io.runnershigh.backend.shared.response
 enum class CommonResponseMessage(
     val message: String,
 ) {
-    SUCCESS_GET_DATA("데이터 조회에 성공했습니다.")
+    SUCCESS_GET_DATA("데이터 조회 성공"),
+    SUCCESS_UPDATE_DATA("데이터 수정 성공"),
+    SUCCESS_DELETE_DATA("데이터 삭제 성공"),
 
 }
