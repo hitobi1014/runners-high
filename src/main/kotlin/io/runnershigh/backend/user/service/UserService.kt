a36505ca99393a8d4d2cdbedb28a00485a3f3e14
minjun <hitobi1014@gmail.com>
2025-06-07 00:31:11 +0900

    [feat] Add 유저 회원가입 Service 구현

--- src/main/kotlin/io/runnershigh/backend/user/service/UserService.kt
+++ src/main/kotlin/io/runnershigh/backend/user/service/UserService.kt
@@ -1,8 +1,12 @@
 package io.runnershigh.backend.user.service
 
 import io.runnershigh.backend.user.dto.request.LoginRequest
+import io.runnershigh.backend.user.dto.request.SignupRequest
 import io.runnershigh.backend.user.dto.response.LoginResponse
 
 interface UserService {
     fun login(request: LoginRequest): LoginResponse
+    fun signup(request: SignupRequest): String
+    fun isLoginIdAvailable(loginId: String): Boolean
+    fun isNicknameAvailable(nickname: String): Boolean
 }
