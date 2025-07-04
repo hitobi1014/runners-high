e09d0b07155e5a9c4c3deca2f367fae9bbf96260
minjun <hitobi1014@gmail.com>
2025-07-05 00:32:03 +0900

    [feat] Modify 훈련일정 저장시 그룹,상세계획 한번에 저장 -> 각 group, items에서 저장하는 함수 주석처리

--- src/main/kotlin/io/runnershigh/backend/training/service/TrainingPlanItemsService.kt
+++ src/main/kotlin/io/runnershigh/backend/training/service/TrainingPlanItemsService.kt
@@ -1,8 +1,5 @@
 package io.runnershigh.backend.training.service
 
-import io.runnershigh.backend.training.dto.request.SaveTrainingPlanItem
-import io.runnershigh.backend.training.entity.TrainingPlanItems
-
 interface TrainingPlanItemsService {
-    fun createTrainingItems(scheduleId: Long, dto: SaveTrainingPlanItem): TrainingPlanItems
+//    fun createTrainingItems(scheduleId: Long, dto: SaveTrainingPlanItem): TrainingPlanItems
 }
--- src/main/kotlin/io/runnershigh/backend/training/service/TrainingPlanItemsServiceImpl.kt
+++ src/main/kotlin/io/runnershigh/backend/training/service/TrainingPlanItemsServiceImpl.kt
@@ -1,13 +1,10 @@
 package io.runnershigh.backend.training.service
 
-import io.runnershigh.backend.training.mapper.toEntity
+import io.runnershigh.backend.training.entity.TrainingSchedules
 import io.runnershigh.backend.training.exception.TrainingException
 import io.runnershigh.backend.training.exception.TrainingExceptionType
-import io.runnershigh.backend.training.entity.TrainingPlanItems
-import io.runnershigh.backend.training.entity.TrainingSchedules
 import io.runnershigh.backend.training.repository.TrainingPlanItemsRepository
 import io.runnershigh.backend.training.repository.TrainingSchedulesRepository
-import io.runnershigh.backend.training.dto.request.SaveTrainingPlanItem
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -20,13 +17,13 @@ class TrainingPlanItemsServiceImpl(
     private val trainingSchedulesRepository: TrainingSchedulesRepository,
 ) : TrainingPlanItemsService {
 
-    override fun createTrainingItems(
-        scheduleId: Long,
-        dto: SaveTrainingPlanItem,
-    ): TrainingPlanItems {
-        val entity = dto.toEntity(getSchedule(scheduleId))
-        return trainingPlanItemsRepository.save(entity)
-    }
+//    override fun createTrainingItems(
+//        scheduleId: Long,
+//        dto: SaveTrainingPlanItem,
+//    ): TrainingPlanItems {
+//        val entity = dto.toEntity(getSchedule(scheduleId))
+//        return trainingPlanItemsRepository.save(entity)
+//    }
 
     private fun getSchedule(scheduleId: Long): TrainingSchedules {
         return trainingSchedulesRepository.findByIdOrNull(scheduleId)
