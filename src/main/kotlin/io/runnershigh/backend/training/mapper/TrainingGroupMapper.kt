8bd01e8a535acad9591915a19360d6103c8462b3
minjun <hitobi1014@gmail.com>
2025-07-05 00:34:30 +0900

    [feat] Modify 훈련 mapper

--- src/main/kotlin/io/runnershigh/backend/training/mapper/TrainingGroupMapper.kt
+++ src/main/kotlin/io/runnershigh/backend/training/mapper/TrainingGroupMapper.kt
@@ -1,10 +1,10 @@
 package io.runnershigh.backend.training.mapper
 
+import io.runnershigh.backend.training.dto.request.SaveTrainingGroup
 import io.runnershigh.backend.training.entity.TrainingPlanGroups
 import io.runnershigh.backend.training.entity.TrainingSchedules
-import io.runnershigh.backend.training.dto.request.SaveTrainingPlanGroup
 
-fun SaveTrainingPlanGroup.toEntity(schedule: TrainingSchedules) = TrainingPlanGroups(
+fun SaveTrainingGroup.toEntity(schedule: TrainingSchedules) = TrainingPlanGroups(
     schedule = schedule,
     groupOrder = this.groupOrder,
     repeatCount = this.repeatCount,
--- src/main/kotlin/io/runnershigh/backend/training/mapper/TrainingScheduleMapper.kt
+++ src/main/kotlin/io/runnershigh/backend/training/mapper/TrainingScheduleMapper.kt
@@ -1,12 +1,12 @@
 package io.runnershigh.backend.training.mapper
 
-import io.runnershigh.backend.training.dto.request.SaveTrainingSchedule
+import io.runnershigh.backend.training.dto.request.SaveTrainingInfo
 import io.runnershigh.backend.training.dto.response.ReadTrainingSchedule
 import io.runnershigh.backend.training.entity.TrainingSchedules
 import io.runnershigh.backend.user.entity.UserEntity
 
 // request
-fun SaveTrainingSchedule.toEntity(user: UserEntity) = TrainingSchedules(
+fun SaveTrainingInfo.toEntity(user: UserEntity) = TrainingSchedules(
     user = user,
     title = this.title,
     location = this.location,
