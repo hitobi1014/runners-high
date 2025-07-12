deddacfac19b6ba92270ceaf07bfd7470351d656
minjun <hitobi1014@gmail.com>
2025-07-12 23:25:49 +0900

    [feat] Modify DateUtil 이전 일요일, 이후 토요일 찾기 로직 제거

--- src/main/kotlin/io/runnershigh/backend/shared/util/DateUtils.kt
+++ src/main/kotlin/io/runnershigh/backend/shared/util/DateUtils.kt
@@ -27,48 +27,18 @@ object DateUtils {
             date.with(TemporalAdjusters.next(DayOfWeek.SUNDAY))
         }
     }
-
-
-    /**
-     * 주어진 날짜 또는 현재 날짜를 기준으로 가장 가까운 이전 일요일 날짜를 반환합니다.
-     *
-     * @param date 기준 날짜 (기본값: 현재 날짜)
-     * @return 가장 가까운 이전 일요일 날짜
-     */
-    fun findPreviousSunday(date: LocalDate = LocalDate.now()): LocalDate {
-        return if (date.dayOfWeek == DayOfWeek.SUNDAY) {
-            date
-        } else {
-            date.with(TemporalAdjusters.previous(DayOfWeek.SUNDAY))
-        }
-    }
-
-    /**
-     * 주어진 날짜 또는 현재 날짜를 기준으로 가장 가까운 이후 토요일 날짜를 반환합니다.
-     *
-     * @param date 기준 날짜 (기본값: 현재 날짜)
-     * @return 가장 가까운 이후 토요일 날짜
-     */
-    fun findNextSaturday(date: LocalDate = LocalDate.now()): LocalDate {
-        return if (date.dayOfWeek == DayOfWeek.SATURDAY) {
-            date
-        } else {
-            date.with(TemporalAdjusters.next(DayOfWeek.SATURDAY))
-        }
-    }
-
-
+    
     /**
      * 주어진 날짜 또는 현재 날짜를 기준으로 해당 주의 경계를 반환합니다.
      *
-     * 경계는 가장 가까운 이전 일요일과 이후 토요일로 정의됩니다.
+     * 경계는 가장 가까운 이전 월요일과 이후 일요일로 정의됩니다.
      *
      * @param date 기준 날짜 (기본값: 현재 날짜)
-     * @return 해당 주의 경계를 나타내는 Pair (이전 일요일, 이후 토요일)
+     * @return 해당 주의 경계를 나타내는 Pair (이전 월요일, 이후 일요일)
      */
     fun getWeekBoundaries(date: LocalDate = LocalDate.now()): Pair<LocalDate, LocalDate> {
-        val previousSunday = findPreviousSunday(date)
-        val nextSaturday = findNextSaturday(date)
-        return previousSunday to nextSaturday
+        val previousMonday = findPreviousMonday(date)
+        val nextSunday = findNextSunday(date)
+        return previousMonday to nextSunday
     }
 }
--- src/test/kotlin/io/runnershigh/backend/shared/util/DateUtilsTest.kt
+++ src/test/kotlin/io/runnershigh/backend/shared/util/DateUtilsTest.kt
@@ -1,38 +1,11 @@
 package io.runnershigh.backend.shared.util
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class DateUtilsTest {
-
-    @Test
-    @DisplayName("현재 날짜가 일요일인 경우 해당 날짜 반환")
-    fun findPreviousSunday_whenCurrendDateIsSunday_returnSameDay() {
-        //given
-        val sunday = LocalDate.of(2025, 4, 13)
-
-        //when
-        val result = DateUtils.findPreviousSunday(sunday)
-
-        //then
-        assertEquals(sunday, result)
-    }
-
-    @Test
-    @DisplayName("현재 날짜가 일요일이 아닌 경우 이전 일요일 반환")
-    fun findPreviousSunday_whenCurrentDateIsNotSunday_returnsPreviousSunday() {
-        //given
-        val wednesday = LocalDate.of(2025, 4, 9)
-        val expectedSunday = LocalDate.of(2025, 4, 6)
-
-        //when
-        val result = DateUtils.findPreviousSunday(wednesday)
-
-        //then
-        assertEquals(expectedSunday, result)
-    }
 
     @Test
     @DisplayName("현재 일자 기준으로 가장 가까운 이전 일요일, 가장 가까운 이후 토요일 반환")
